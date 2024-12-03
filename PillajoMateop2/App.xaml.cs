f06cfe7d8df4e0b8cfa528123faf95e139272b53
laboratorio <laboratorio@UPOAULA10318>
2024-12-03 07:31:20 -0500

    Agregar archivos de proyecto.

---
+++ PillajoMateop2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PillajoMateop2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
